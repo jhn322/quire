2155e4baeeacb7e6e771899cfc98d492714f2cde
MustafaAltaie <mustafaphoto111@gmail.com>
2024-01-19 12:11:23 +0100

    create new note

--- js/notes.js
+++ js/notes.js
@@ -19,11 +19,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  const noteTitle = document.getElementById('noteTitle');
+  const noteField = document.getElementById('note-field');
+  const noteList = document.querySelector('.note-list');
+
   // save to local storage when pressing button
   saveBtn.addEventListener("click", function () {
     const newNotes = notes.value;
 
     localStorage.setItem("notes", newNotes);
     saveBtn.classList.add("hide-btn");
+
+    let newNote = `
+      <li class="note-thumbnail">
+        <h3>${noteTitle.value}</h3>
+        <p>${noteField.value}</p>
+      </li>
+    `;
+    noteList.innerHTML += newNote;
   });
 });
