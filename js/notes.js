c09f7584519961850b114ebbdbce7823a32294c4
Alexander Kuiper <kuiper9000@gmail.com>
2024-01-24 13:39:52 +0100

    fixed conflict and updated some parts

--- js/notes.js
+++ js/notes.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   // check local storage for stored notes
-  const storedNotes = localStorage.getItem("notes");
+  const storedNotes = JSON.parse(localStorage.getItem("notes")) || {};
 
   // update notes
   const notes = document.getElementById("note-field");
-  notes.textContent = storedNotes;
+  notes.textContent = storedNotes.noteText;
+  const title = document.getElementById("noteTitle");
+  title.value = storedNotes.noteTitle || "";
 
   // button to save
   const saveBtn = document.getElementById("save-notes");
@@ -19,28 +21,46 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  const noteTitle = document.getElementById("noteTitle");
+  const noteField = document.getElementById("note-field");
+  const noteList = document.querySelector(".note-list");
+  const addNote = document.querySelector(".add-note");
+
+  addNote.addEventListener("click", () => {
+    noteTitle.value = "";
+    noteField.value = "";
+  });
+
   // save to local storage when pressing button
   saveBtn.addEventListener("click", function () {
-    const newNotes = notes.value;
+    let newNotes = {
+      noteTitle: title.value,
+      noteText: notes.value,
+    };
 
-    localStorage.setItem("notes", newNotes);
+    localStorage.setItem("notes", JSON.stringify(newNotes));
     saveBtn.classList.add("hide-btn");
-    saveAllNotes(notes);
 
+    let newNote = `
+      <li class="note-thumbnail">
+        <h3>${noteTitle.value}</h3>
+        <p>${noteField.value}</p>
+      </li>
+    `;
+    noteList.innerHTML += newNote;
   });
+  saveAllNotes(notes);
 });
 
-let savedNotes=[];
-function saveAllNotes(notes){
+let savedNotes = [];
+function saveAllNotes(notes) {
+  let testNote = {
+    title: "variable here",
+    content: localStorage.getItem("notes"),
+  };
+  savedNotes.push(testNote);
 
-
-let testNote = {
-  title: "variable here" ,
-  content : localStorage.getItem("notes")
-}
-savedNotes.push(testNote)
-
-console.log(JSON.stringify(savedNotes))
-let test = JSON.stringify(savedNotes);
-console.log(JSON.parse(test));
+  console.log(JSON.stringify(savedNotes));
+  let test = JSON.stringify(savedNotes);
+  console.log(JSON.parse(test));
 }
