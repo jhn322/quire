4376f5b16f7c5d172c3caa8071ee3ba5efa02289
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-01-26 11:38:10 +0100

    added basic functionality for formatting text in textfield to bold, italic and underlined

--- js/toolbar.js
+++ js/toolbar.js
@@ -4,13 +4,13 @@ const toolIcons = document.querySelectorAll('.tool-icon');
 //variable for note field
 const noteField = document.getElementById('note-field');
 
-//get notes from localStorage
-// const notes = JSON.parse(localStorage.getItem('notes')) || {};
-
 //variable to check if icon is already selected
-let isBoldActive = false;
-let isItalicActive = false;
-let isUnderlineActive = false;
+let isBold = false;
+let isItalic = false;
+let isUnderline= false;
+
+let selectedText = null;
+let range = null;
 
 toolIcons.forEach(function(toolIcon){
     toolIcon.addEventListener('click', (event)=> {
@@ -21,22 +21,13 @@ toolIcons.forEach(function(toolIcon){
         
         switch(toolIconId) {
             case 'bold':
-                isBoldActive = !isBoldActive;
-                console.log(isBoldActive);
-                if(isBoldActive) {
-                    console.log("Nu ska texten bli fet");
-                    makeTextBold();
-                } else {
-                    removeBold();
-                }
+                toggleBold();
                 break;
             case 'italic':
-                isItalicActive = !isItalicActive;
-                console.log("Nu ska texten bli kursiv");
+                toggleItalic();
                 break;
             case 'underline':
-                isUnderlineActive = !isUnderlineActive;
-                console.log("Nu ska texten bli understruken");
+                toggleUnderline();
                 break;
             case 'ul-list':
                 console.log("Nu ska vi skapa en ul-list");
@@ -48,31 +39,132 @@ toolIcons.forEach(function(toolIcon){
                 console.log("Nu ska vi lägga till en bild");
                 break;
         }
-        toolIcon.classList.toggle('tool-icon_active');
     })
 })
 
-function makeTextBold() {
-    //code to get selected text
-    const selectedText = window.getSelection().toString();
-    console.log(selectedText);
-    console.log(typeof selectedText);
+//eventlistener on mouse up and key combination of shift and arrow
+noteField.addEventListener('mouseup', checkSelectedText);
+noteField.addEventListener('keyup', function(event) {
+    if(event.shiftKey && event.key.includes('Arrow')) {
+        checkSelectedText();
+    }
+})
 
-    const formattedText = `<strong>${selectedText}</strong>`;
-    console.log(formattedText);
+//function to check and save selected text
+function checkSelectedText() {
+    let selection = window.getSelection().toString();
+    
+    if (selection.length > 0) {
+        selectedText = selection.toString();
+        range = window.getSelection().getRangeAt(0);
+        console.log('Markerad text: ' + selectedText);
+    }
+    return selection;
+};
 
-    let text = noteField.innerHTML;
-    noteField.innerHTML = text.replace(selectedText, formattedText); 
+//function to switch between bold and normal text
+function toggleBold() {
+    const boldBtn = document.getElementById('bold');
+
+    selection = checkSelectedText()
+
+    if (selection.length < 1) {
+        alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
+        return;
+    }
+
+    //create a span element
+    const span = document.createElement('span');
+    console.log(span);
+
+    //check if bold is already applied
+    if(isBold) {
+        span.style.fontWeight = "normal";
+        boldBtn.classList.remove('tool-icon_active');
+    } else {
+        span.style.fontWeight = "bold";
+        boldBtn.classList.add('tool-icon_active');
+    }
+
+    //add the selected text to span element
+    span.innerHTML = selectedText;
+
+    //replace range content
+    range.deleteContents();
+    range.insertNode(span);
+
+    //toggle isBold
+    isBold = !isBold;
 }
 
+//function to switch between italic and normal text
+function toggleItalic() {
+    const italicBtn = document.getElementById('italic');
 
-function removeBold() {
-    //code to get selected text
-    const selectedText = window.getSelection().toString();
-    console.log(selectedText);
-    console.log(typeof selectedText);
+    selection = checkSelectedText()
 
-    console.log("Nu ska den feta stilen bort!");
+    if (selection.length < 1) {
+        alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
+        return;
+    }
+
+    //create a span element
+    const span = document.createElement('span');
+    console.log(span);
+
+    //check if bold is already applied
+    if(isItalic) {
+        span.style.fontStyle = "normal";
+        italicBtn.classList.remove('tool-icon_active');
+    } else {
+        span.style.fontStyle = "italic";
+        italicBtn.classList.add('tool-icon_active');
+    }
+
+    //add the selected text to span element
+    span.innerHTML = selectedText;
+
+    //replace range content
+    range.deleteContents();
+    range.insertNode(span);
+
+    //toggle isBold
+    isItalic = !isItalic;
+}
+
+//function to switch between underlined and normal text
+function toggleUnderline() {
+    const underlineBtn = document.getElementById('underline');
+
+    selection = checkSelectedText()
+
+    if (selection.length < 1) {
+        alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
+        return;
+    }
+
+    //create a span element
+    const span = document.createElement('span');
+    console.log(span);
+
+    //check if bold is already applied
+    if(isUnderline) {
+        span.style.textDecoration = "none";
+        underlineBtn.classList.remove('tool-icon_active');
+    } else {
+        span.style.textDecoration = "underline";
+        underlineBtn.classList.add('tool-icon_active');
+    }
+
+    //add the selected text to span element
+    span.innerHTML = selectedText;
+
+    //replace range content
+    range.deleteContents();
+    range.insertNode(span);
+
+    //toggle isUnderlined
+    isUnderline = !isUnderline;
 }
 
 
