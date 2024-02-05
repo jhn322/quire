03246ef900439a15025487c4e78504712b8352d5
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-02-05 17:01:20 +0100

    added comments and worked on code structur

--- js/toolbar.js
+++ js/toolbar.js
@@ -4,24 +4,23 @@ const toolIcons = document.querySelectorAll('.tool-icon');
 //variable for note field
 const noteField = document.getElementById('note-field');
 
+//Eventlisteners for all toolbar buttons
 toolIcons.forEach(function(toolIcon){
     toolIcon.addEventListener('click', (event)=> {
         const { target } = event;
         const toolIconId = target.id; 
-
+        
         event.preventDefault();
 
+        //function to get user selected text
         let selectionInfo = checkSelectedText();
         
+        //switch statement to call function depending on user choice
         switch(toolIconId) {
             case 'bold':
-                toggleBold(selectionInfo);
-                break;
             case 'italic':
-                toggleItalic(selectionInfo);
-                break;
             case 'underline':
-                toggleUnderline(selectionInfo);
+                toggleStyle(toolIconId, selectionInfo);
                 break;
             case 'ul-list':
                 toggleUnorderedlist(selectionInfo);
@@ -36,41 +35,34 @@ toolIcons.forEach(function(toolIcon){
     })
 })
 
-//function to check and save selected text
+//TODO - eventlisteners for all select menus
+
+// ----------- function to get selected text -----------------
 function checkSelectedText() {
     let selection = window.getSelection();
     
     if (selection && selection.toString().length > 0) {
         let range = selection.getRangeAt(0);
-        let startOffset = range.startOffset;
-        let endOffset = range.endOffset;
-
+        
         return {
-            selection: selection,
             selectedText: selection.toString(),
             range: range,
-            startOffset: startOffset,
-            endOffset: endOffset,
-            textBeforeSelection: noteField.textContent.substring(0, startOffset),
-            textAfterSelection: noteField.textContent.substring(endOffset),
-            stringToSearch: noteField.innerHTML.slice(startOffset, endOffset)
         };
     }
 }
 
 // -----------------------------------------------------------------------------------
-// ------  Bold section -----------------------------------------------------
+// ------  Toggle style section -----------------------------------------------------
 // -----------------------------------------------------------------------------------
-//function to switch between bold and normal text
-function toggleBold(selectionInfo) {
-    
+//function to switch between adding and removing styles
+function toggleStyle(tool, selectionInfo) {
+
     if(!selectionInfo) {
         alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
         return;
     }
     
     let spanElement = null;
-    
     let commonAncestorContainer = selectionInfo.range.commonAncestorContainer;
 
     while (commonAncestorContainer) {
@@ -80,170 +72,74 @@ function toggleBold(selectionInfo) {
         }
         commonAncestorContainer = commonAncestorContainer.parentNode;
     }
-    console.log("gemensam container:", commonAncestorContainer);
 
     if (!spanElement) {
         spanElement = document.createElement('span');
-        spanElement.style.fontWeight = 'bold';
+
+        switch(tool) {
+            case 'bold':
+                spanElement.style.fontWeight = 'bold';
+                break;
+            case 'italic':
+                spanElement.style.fontStyle = 'italic';
+                break;
+            case 'underline':
+                spanElement.style.textDecoration = 'underline';
+                break;
+        }
         selectionInfo.range.surroundContents(spanElement);
     } else {
-        const style = window.getComputedStyle(spanElement);
-        console.log('Fetstil:', style.fontWeight);
-        console.log('Kursiv stil:', style.fontStyle);
-        console.log('Understruken stil:', style.textDecoration);
+        const styleAttribute = window.getComputedStyle(spanElement);
         
-        if(style.fontWeight.includes('700')){
-            spanElement.style.fontWeight = '400';
-            console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
-        } else if (style.fontWeight.includes('400')){
-            spanElement.style.fontWeight = '700';
-            console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
+        switch(tool) {
+            case 'bold':
+                toggleBold(styleAttribute, spanElement);
+                break;
+            case 'italic':
+                toggleItalic(styleAttribute, spanElement);
+                break;
+            case 'underline':
+                toggleUnderline(styleAttribute, spanElement);
+                break;
         }
+    }
+    // Deselect the text
+    window.getSelection().removeAllRanges();
+
+    console.log(noteField.innerHTML);
+}
+
+// ----------- toggle bold function ----------------------
+function toggleBold(style, span) {
+    if(style.fontWeight.includes('700')){
+        span.style.fontWeight = '400';
+        console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
+    } else if (style.fontWeight.includes('400')){
+        span.style.fontWeight = '700';
+        console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
     }
 }
 
-// -----------------------------------------------------------------------------------
-// ------  Italic section -----------------------------------------------------
-// -----------------------------------------------------------------------------------
-//function to switch between italic and normal text
-function toggleItalic(selectionInfo) {
-    
-    if(!selectionInfo) {
-        alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
-        return;
-    }
-
-    let spanElement = null;
-    
-    let commonAncestorContainer = selectionInfo.range.commonAncestorContainer;
-
-    while (commonAncestorContainer) {
-        if (commonAncestorContainer.nodeName === 'SPAN' && commonAncestorContainer.textContent.trim() === selectionInfo.selectedText) {
-            spanElement = commonAncestorContainer;
-            break;
-        }
-        commonAncestorContainer = commonAncestorContainer.parentNode;
-    }
-    console.log("gemensam container:", commonAncestorContainer);
-
-    if (!spanElement) {
-        spanElement = document.createElement('span');
-        spanElement.style.fontStyle = 'italic';
-        selectionInfo.range.surroundContents(spanElement);
-    } else {
-        const style = window.getComputedStyle(spanElement);
-        console.log('Fetstil:', style.fontWeight);
-        console.log('Kursiv stil:', style.fontStyle);
-        console.log('Understruken stil:', style.textDecoration);
-        
-        if(style.fontStyle.includes('italic')){
-            spanElement.style.fontStyle = 'normal';
-            console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
-        } else if (style.fontStyle.includes('normal')){
-            spanElement.style.fontStyle = 'italic';
-            console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
-        }
+// ----------- toggle italic function ----------------------
+function toggleItalic(style, span) {
+    if(style.fontStyle.includes('italic')){
+        span.style.fontStyle = 'normal';
+        console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
+    } else if (style.fontStyle.includes('normal')){
+        span.style.fontStyle = 'italic';
+        console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
     }
 }
 
-// -----------------------------------------------------------------------------------
-// ------  Underline section -----------------------------------------------------
-// -----------------------------------------------------------------------------------
-//function to switch between underlined and normal text
-function toggleUnderline(selectionInfo) {
-
-    if(!selectionInfo) {
-        alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
-        return;
+// ----------- toggle underline function ----------------------
+function toggleUnderline(style, span) {
+    if(style.textDecoration.includes('underline')){
+        span.style.textDecoration = 'none';
+        console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
+    } else if (style.textDecoration.includes('none')){
+        span.style.textDecoration = 'underline';
+        console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
     }
-
-    let spanElement = null;
-    
-    let commonAncestorContainer = selectionInfo.range.commonAncestorContainer;
-
-    while (commonAncestorContainer) {
-        if (commonAncestorContainer.nodeName === 'SPAN' && commonAncestorContainer.textContent.trim() === selectionInfo.selectedText) {
-            spanElement = commonAncestorContainer;
-            break;
-        }
-        commonAncestorContainer = commonAncestorContainer.parentNode;
-    }
-    console.log("gemensam container:", commonAncestorContainer);
-
-    if (!spanElement) {
-        spanElement = document.createElement('span');
-        spanElement.style.textDecoration = 'underline';
-        selectionInfo.range.surroundContents(spanElement);
-    } else {
-        const style = window.getComputedStyle(spanElement);
-        console.log('Fetstil:', style.fontWeight);
-        console.log('Kursiv stil:', style.fontStyle);
-        console.log('Understruken stil:', style.textDecoration);
-        
-        if(style.textDecoration.includes('underline')){
-            spanElement.style.textDecoration = 'none';
-            console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
-        } else if (style.textDecoration.includes('none')){
-            spanElement.style.textDecoration = 'underline';
-            console.log('Efter: understruken:', style.textDecoration, 'kursiv:', style.fontStyle, 'fetstil:', style.fontWeight);
-        }
-    }
-}
-
-// -----------------------------------------------------------------------------------
-// ------   Type of Text Section -----------------------------------------------------
-// -----------------------------------------------------------------------------------
-const selectTextType = document.getElementById('text-type');
-
-selectTextType.addEventListener('change', function(event){
-    event.preventDefault();
-    changeTextType(event);
-});
-
-function changeTextType(event) {
-
-    let selectionInfo = checkSelectedText();
-
-    if(!selectionInfo) {
-        alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
-        return;
-    }
-    
-    let { target } = event;
-    let { value } = target;
-
-    let textTagElement = null;
-    
-    let commonAncestorContainer = selectionInfo.range.commonAncestorContainer;
-
-    const textTags = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P'];
-
-    while (commonAncestorContainer) {
-        if (textTags.includes(commonAncestorContainer.nodeName) && commonAncestorContainer.textContent.trim() === selectionInfo.selectedText){
-            textTagElement = commonAncestorContainer;
-            break;
-        }
-        commonAncestorContainer = commonAncestorContainer.parentNode;
-    }
-    console.log("gemensam container:", commonAncestorContainer);
-
-    if (!textTagElement) {
-        textTagElement = document.createElement(`${value}`);
-        selectionInfo.range.surroundContents(textTagElement);
-        console.log(textTagElement);
-    } else {
-        const newTagElement = document.createElement(`${value}`);
-        newTagElement.innerHTML = textTagElement.innerHTML;
-        textTagElement.parentNode.replaceChild(newTagElement, textTagElement);
-    }
-
-    document.addEventListener('keyup', function(event){
-        if(event.key === 'Enter') {
-           //TODO - O.b.s! Koden nedan behövs när en ny anteckningen skapas!
-           const defaultTextIndex = 6;
-           selectTextType.selectedIndex = defaultTextIndex;
-        }
-    });
 }
 
 // -----------------------------------------------------------------------------------
@@ -317,6 +213,64 @@ function createRangeAtCursor() {
   newRange.setEnd(range.startContainer, currentOffset);
   return newRange;
 }
+
+// -----------------------------------------------------------------------------------
+// ------   Type of Text Section -----------------------------------------------------
+// -----------------------------------------------------------------------------------
+const selectTextType = document.getElementById('text-type');
+
+selectTextType.addEventListener('change', function(event){
+    event.preventDefault();
+    changeTextType(event);
+});
+
+function changeTextType(event) {
+
+    let selectionInfo = checkSelectedText();
+
+    if(!selectionInfo) {
+        alert("Du har ingen text markerad.\nMarkera den text du vill ändra och prova igen.");
+        return;
+    }
+    
+    let { target } = event;
+    let { value } = target;
+
+    let textTagElement = null;
+    
+    let commonAncestorContainer = selectionInfo.range.commonAncestorContainer;
+
+    const textTags = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P'];
+
+    while (commonAncestorContainer) {
+        if (textTags.includes(commonAncestorContainer.nodeName) && commonAncestorContainer.textContent.trim() === selectionInfo.selectedText){
+            textTagElement = commonAncestorContainer;
+            break;
+        }
+        commonAncestorContainer = commonAncestorContainer.parentNode;
+    }
+    console.log("gemensam container:", commonAncestorContainer);
+
+    if (!textTagElement) {
+        textTagElement = document.createElement(`${value}`);
+        selectionInfo.range.surroundContents(textTagElement);
+        console.log(textTagElement);
+    } else {
+        const newTagElement = document.createElement(`${value}`);
+        newTagElement.innerHTML = textTagElement.innerHTML;
+        textTagElement.parentNode.replaceChild(newTagElement, textTagElement);
+    }
+
+    document.addEventListener('keyup', function(event){
+        if(event.key === 'Enter') {
+           //TODO - O.b.s! Koden nedan behövs när en ny anteckningen skapas!
+           const defaultTextIndex = 6;
+           selectTextType.selectedIndex = defaultTextIndex;
+        }
+    });
+}
+
+
 
 // // -----------------------------------------------------------------------------------
 // // ------   Font-family Section -----------------------------------------------------
