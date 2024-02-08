79771a70d96dbe28686836b30f52f76b672101c3
Johan Söderlund <johan.soderlund96@gmail.com>
2024-02-08 17:29:08 +0100

    formatting

--- js/googleAnalytics.js
+++ js/googleAnalytics.js
@@ -1,7 +1,5 @@
 // Gtag for search icon - Johan.S
-const searchIcon = document.querySelector(
-  ".search-icon.fa-solid.fa-magnifying-glass"
-);
+const searchIcon = document.querySelector(".search-icon.fa-solid.fa-magnifying-glass");
 
 searchIcon.addEventListener("click", function () {
   // Custom gtag event when the search icon is clicked
@@ -13,13 +11,13 @@ searchIcon.addEventListener("click", function () {
 // ------------------------------------------------------
 
 //Gtag for new-note-button - Ellen
-const addNoteBtn = document.querySelector('.add-note');
+const addNoteBtn = document.querySelector(".add-note");
 
-//eventListener with custom gtag event for click on new note 
-addNoteBtn.addEventListener('click', function(){
-  gtag('event', 'Add_new_note', {
-    'event_category': 'click_engagement',
-    'event_label': 'new_note_click'
+//eventListener with custom gtag event for click on new note
+addNoteBtn.addEventListener("click", function () {
+  gtag("event", "Add_new_note", {
+    event_category: "click_engagement",
+    event_label: "new_note_click",
   });
 });
 // ------------------------------------------------------
