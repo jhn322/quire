0100e63194f94dc2bf79ab712138a679019ca099
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2024-02-09 16:11:04 +0100

    changes are implemented from other branch

--- js/notes.js
+++ js/notes.js
@@ -146,7 +146,7 @@ function renderAllTumbnails() {
 function getDate(){
   const date = new Date()
   //return `${date.getFullYear()}-${((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1)}-${(date.getDate() < 10 ? '0' : '') + date.getDate()}`
-  return `${monthsList[date.getMonth()]} ${(date.getDate() < 10 ? '0' : '') + date.getDate()}, ${date.getFullYear()}`
+  return `${monthsList[date.getMonth()]} ${(date.getDate() < 10 ? '0' : '') + date.getDate()}, ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 
 }
 
